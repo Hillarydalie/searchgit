6b1a9879d1df13ee4004bee32646437ff38f451b
hillarydalie <hidalie@gmail.com>
2019-08-25 09:29:38 +0300

    Create a div to test our api

--- src/app/tafuta/tafuta.component.ts
+++ src/app/tafuta/tafuta.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import {TafutaHttpService} from '../services/tafuta-http.service';
+import {Gitsearch} from '../gitsearch';
+ 
 @Component({
   selector: 'app-tafuta',
   templateUrl: './tafuta.component.html',
   styleUrls: ['./tafuta.component.css']
 })
+
 export class TafutaComponent implements OnInit {
 
-  constructor() { }
+    gitsearches:Gitsearch[];
+    constructor(public tafutaHttpService:TafutaHttpService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        // this.searchGithub(" ");
+    }
+
+    searchGithub(searchWord){
+        this.tafutaHttpService.searchingGits(searchWord).then(
+            ()=>{
+                this.gitsearches=this.tafutaHttpService.gitsearches;
+                // console.log(this.gitsearches)
+            },
+            (error)=>{
+                console.log(error)
+            }
+        )
+    }
 
 }
