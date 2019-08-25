0e9bc96b68865d263377698bed538269ec4746f9
hillarydalie <hidalie@gmail.com>
2019-08-25 09:32:46 +0300

    Conclude the testing process of the api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 import { TafutaFormComponent } from './tafuta-form/tafuta-form.component';
 import { TafutanavComponent } from './tafutanav/tafutanav.component';
 import { TafutaComponent } from './tafuta/tafuta.component';
@@ -18,7 +20,9 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
